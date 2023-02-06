68a21a534f8cfef15fab76bb9d5e49918640f4d3
Rodrigo D'Ambrosio <rodrigo.dambrosio@gmail.com>
2023-02-06 11:28:42 -0300

    Add ToDatatable method

--- Functions.cs
+++ Functions.cs
@@ -1,4 +1,4 @@
-﻿using FastMember;
+using FastMember;
 using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
@@ -20,6 +20,25 @@ namespace TableValuedParametersExample
             using (var reader = ObjectReader.Create(data, "Id", "FirstName", "LastName","Number","PhotoPath","Wins"))
             {
                 table.Load(reader);
+            }
+            return table;
+        }
+        // This is a more generic method
+        public static DataTable ToDataTable<T>(IEnumerable<T> list)
+        {
+            // https://stackoverflow.com/questions/564366/convert-generic-list-enumerable-to-datatable
+            IEnumerable<T> data = list;
+
+            DataTable table = new();
+            // It needs to have the same order of the SQL Type 
+            try
+            {
+                using var reader = ObjectReader.Create(data, typeof(T).GetProperties().Select(x => x.Name).ToArray());
+                table.Load(reader);
+            }
+            catch (Exception ex)
+            {
+                //Log.Error(ex.ToString());
             }
             return table;
         }
